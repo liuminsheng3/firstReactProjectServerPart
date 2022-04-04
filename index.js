24c7762bc262f4306d5c357b2eeba61a7de745f3
liuminsheng3 <liuminsheng3@gmail.com>
2022-04-05 00:51:26 +0800

    set cookie,  maxtime is 30 days, and key are customized string

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const { cookie } = require('express/lib/response');
 
 require("./models/User");
 require('./services/passport');
@@ -8,6 +11,16 @@ require('./services/passport');
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
