bb4bf05f560ddc91ce6e3c6b31efa237affa74a5
liuminsheng3 <liuminsheng3@gmail.com>
2022-04-04 17:07:53 +0800

    add connection to mongoDB and serializtion & deserialization user function

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+
+require("./models/User");
 require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
 require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5001;
-
 app.listen(PORT);
 
--- services/passport.js
+++ services/passport.js
@@ -3,7 +3,21 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('../config/keys');
 const HttpsProxyAgent = require('https-proxy-agent');
 const agent = new HttpsProxyAgent(process.env.HTTP_PROXY || "http://127.0.0.1:8889");
+const mongoose = require('mongoose');
+const { download } = require('express/lib/response');
 
+const User = mongoose.model("users");
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id)
+    .then(user =>{
+      download(null, user);
+    });
+});
 
 const  gStrategy =  new GoogleStrategy(
     {
@@ -11,10 +25,26 @@ const  gStrategy =  new GoogleStrategy(
       clientSecret: keys.googleClientSecret,
       callbackURL: '/auth/google/callback'
     },
+    //user has successfully signed in on google.
     (accessToken, refreshToken, profile, done) => {
-      console.log('access token', accessToken);
-      console.log('refresh token', refreshToken);
-      console.log('profile:', profile);
+      console.log("successfully login on google!");
+      User.findOne({googleID: profile.id})
+       .then((existingUser) => {
+          if(existingUser){
+            //we already have a record with the given ID
+            console.log("this user already has an account");
+            console.log(existingUser);
+
+            done(null, existingUser);
+          }else{
+            //we don't have a user record with this ID,
+            //make a new user
+            console.log("this user doesn't have an account, create new one!");
+            new User({googleID: profile.id}).save()
+              .then(user => done(null, user));
+
+          }
+       })
     }
   );
 gStrategy._oauth2.setAgent(agent);
