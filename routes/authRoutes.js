3324383279ee8aa7cde086c7544a467182588ed3
liuminsheng3 <liuminsheng3@gmail.com>
2022-04-05 01:38:40 +0800

    handle logout request

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,6 +2,7 @@ const passport  = require('passport');
 
 module.exports = (app) => {
 
+  //request to google auth.
   app.get(
       '/auth/google',
       passport.authenticate('google', {
@@ -9,10 +10,18 @@ module.exports = (app) => {
       })
     );
     
+  // when google auth successed, and return to this url.
   app.get('/auth/google/callback', passport.authenticate('google'));
 
+  //handle when user use cookie info to request data.
   app.get('/api/current_user', (req, res) => {
-    console.log(req);
+
     res.send(req.user);
+  });
+
+  //log out
+  app.get('/api/logout', (req, res) =>{
+    req.logout();
+    res.send("you have successfully logged out");
   });
 }
