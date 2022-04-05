f9733fddb10e99479c3878d2e5cf0eb0591a1304
liuminsheng3 <liuminsheng3@gmail.com>
2022-04-05 16:09:58 +0800

    add production environment, which is run on heroku.

---
+++ config/prod.js
@@ -0,0 +1,15 @@
+// prod.js - production keys here!!
+
+module.exports = {
+    googleClientID:  process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    mongoURI: process.env.MONGO_URL,
+    cookieKey: process.env.COOKIE_KEY
+}
+
+// prod mongoDB username: username
+// prod mongoDB password: WPZvHiiQJsOmbHyk
+
+// prod URL: mongodb+srv://username:WPZvHiiQJsOmbHyk@cluster0.9q02y.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+// prod google client id: 855785401240-82679gtsagr76pci66q0qsj8a4rekqti.apps.googleusercontent.com
+// prod google secret: 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
