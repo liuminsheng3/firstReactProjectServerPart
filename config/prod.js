d8276cfd86f0b6d3b78bb8691bddee7318ebebc7
liuminsheng3 <liuminsheng3@gmail.com>
2022-04-07 18:57:08 +0800

    fix the comment leak of google api secret

--- config/prod.js
+++ config/prod.js
@@ -7,9 +7,3 @@ module.exports = {
     cookieKey: process.env.COOKIE_KEY
 }
 
-// prod mongoDB username: username
-// prod mongoDB password: WPZvHiiQJsOmbHyk
-
-// prod URL: mongodb+srv://username:WPZvHiiQJsOmbHyk@cluster0.9q02y.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
-// prod google client id: 855785401240-82679gtsagr76pci66q0qsj8a4rekqti.apps.googleusercontent.com
-// prod google secret: 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
