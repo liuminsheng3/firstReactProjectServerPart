0a0dd6b73ff0a81175ff87255d942bfe8e0974da
liuminsheng3 <liuminsheng3@gmail.com>
2022-04-10 21:29:26 +0800

    add materializecss libaray for make our use css easier

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,9 +4,17 @@ import React, {Component} from 'react';
 class Header extends Component {
      render() {
         return (
-            <div>
-                Header
+            <nav>
+            <div className="nav-wrapper">
+              <a  className="left brand-logo">Emaily</a>
+              <ul  className="right">
+                <li>
+                    <a >Login with Google</a>
+                </li>
+              </ul>
             </div>
+          </nav>
+                
         );
      }
 }
