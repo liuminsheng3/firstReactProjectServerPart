44cce8ec0ac1d6ee0c8be7796096e8675cdc0cbd
liuminsheng3 <liuminsheng3@gmail.com>
2022-04-11 15:38:23 +0800

    use redux to store user auth state. set up reducer, and use redux-thunk to handle dispatch in createAction function

--- client/src/index.js
+++ client/src/index.js
@@ -3,14 +3,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import {Provider} from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
+import { createRoot } from 'react-dom/client';
+import reduxThunk from 'redux-thunk';
 
 import App from './components/App';
 import reducers from "./reducers";
 
 
-const store = createStore(()=>reducers, {}, applyMiddleware());
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+const container = document.querySelector('#root');
 
-ReactDOM.render(
-    <Provider store = {store}><App/></Provider>,
-    document.querySelector('#root')
+const root = createRoot(container);
+root.render(
+    <Provider store = {store}><App/></Provider>
     );
